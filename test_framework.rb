a3dba937ee655973ecf1cf37b7101983299beb58
Tom Stuart <tom@therye.org>
2011-08-10 15:28:46 +0100

    Kill a blank line

--- test_framework.rb
+++ test_framework.rb
@@ -7,7 +7,6 @@ rescue => err
   print "F"
   @failures << err
 end
-
 at_exit do
   puts "\n\n#{@failures.size == 0 ? "PASS" : "FAIL"}! #{@successes + @failures.size} tests, #{@successes} pass, #{@failures.size} fail"
   puts "\n" + "Failures:\n" + @failures.map {|f| f.message + "\n" + f.backtrace.join("\n")}.join("\n\n") if @failures.size > 0
